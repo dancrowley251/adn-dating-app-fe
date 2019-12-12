5919a4f4ede39c35aac36a8c819f1f8c8cf15e5a
vagrant <vagrant@vagrant.vm>
2019-12-12 21:32:37 +0000

    Minor fix for missing semi-colon

--- src/app/services/error.interceptor.ts
+++ src/app/services/error.interceptor.ts
@@ -31,7 +31,7 @@ export class ErrorInterceptor implements HttpInterceptor {
           return throwError(modelStateErrors || serverError || 'Server Error');
         }
       })
-    )
+    );
   }
 }
 
